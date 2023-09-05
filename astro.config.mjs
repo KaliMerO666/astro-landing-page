344e01194c9b09652642fd99a45a7de9ddd04786
KaliMerO666 <kalimero@ik.me>
2023-09-05 14:57:31 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import media from "astro-media/integration";
+
 
 
 // https://astro.build/config
@@ -11,6 +13,7 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+     media(),
   ],
   vite: {
     ssr: {
