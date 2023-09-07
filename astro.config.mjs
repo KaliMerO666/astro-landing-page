86968f7fae818bd2b9cb1e525567a6ca4473876f
kalimerO666 <kalimero@ik.me>
2023-09-07 21:23:09 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,15 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import media from "astro-media/integration";
-
-
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://station-service.netlify.app",
+  site: "https://kalimeroland.vercel.app/",
   integrations: [
     tailwind(),
-    [astroImageTools],
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
-     media(),
   ],
   vite: {
     ssr: {
